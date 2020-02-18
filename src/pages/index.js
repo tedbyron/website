21268f88f6f345cdb84a7bbdf89f736d99b3d286
Theodore Byron <ted@tedbyron.com>
2020-02-18 09:51:16 -0500

    Add font-awesome; update button

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,14 @@ export default () => (
   <Layout>
     <h1 className="title is-1 has-text-centered">Teddy Byron</h1>
     <h2 className="subtitle is-5 has-text-centered">site's under construction, sorry <span role="img" aria-label="grimacing emoji">😬</span></h2>
-    <nav className="level is-mobile">
-      <div className="level-item">
-        <a href="https://github.com/tedbyron" className="button">github</a>
-      </div>
-    </nav>
+
+    <div className="buttons is-centered">
+      <a href="https://github.com/tedbyron" className="button is-light">
+        <span className="icon">
+          <i className="fab fa-github"></i>
+        </span>
+        <span>tedbyron</span>
+      </a>
+    </div>
   </Layout>
 )
