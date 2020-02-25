b6b0f8640e9c5581408e7a0fa6410528d49dfd31
Theodore Byron <ted@tedbyron.com>
2020-02-25 01:42:14 -0500

    Update 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -11,7 +11,7 @@ const NotFoundPage = () => (
       <div className="container has-text-centered">
         <h1 className="title is-1">404</h1>
         <h2 className="subtitle">
-          That route doesn&apos;t exist...
+          {'That route doesn\'t exist... '}
           <span role="img" aria-label="frowning face with open mouth emoji">😦</span>
         </h2>
         {/* TODO: search bar? */}
