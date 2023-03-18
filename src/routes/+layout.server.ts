b91cacf75595cbb1966b72a967aaabd5b91ed162
Teddy Byron <ted@tedbyron.com>
2023-03-18 17:02:31 -0400

    update layout server load

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,8 +1,8 @@
 import type { LayoutServerLoad } from './$types'
 
-export const load: LayoutServerLoad = ({ setHeaders }) => {
+export const load = (({ setHeaders }) => {
   setHeaders({
     'X-Frame-Options': 'DENY',
     'Referrer-Policy': 'origin-when-cross-origin, strict-origin-when-cross-origin'
   })
-}
+}) satisfies LayoutServerLoad
