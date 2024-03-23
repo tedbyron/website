7f70b2ddfbea2058510e1998f816741122e96530
Teddy Byron <ted@ted.ooo>
2024-03-23 15:40:44 -0400

    Revert "Update fonts, eslintrc"
    This reverts commit 3a33620ace576e2528c0af15d2f19191c9643248.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,7 +7,6 @@ module.exports = {
     'plugin:@typescript-eslint/strict-type-checked',
     'plugin:@typescript-eslint/stylistic-type-checked',
     'plugin:svelte/recommended',
-    'plugin:svelte/prettier',
     'prettier',
   ],
   parser: '@typescript-eslint/parser',
