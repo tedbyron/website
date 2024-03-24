6fe9e8089f8fe9e4b5a58f3cdc5ef44aea0eb755
Teddy Byron <ted@ted.ooo>
2024-03-24 17:18:29 -0400

    Update svelte config to make objects immutable by default

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
+  compilerOptions: { immutable: true },
   preprocess: vitePreprocess(),
   kit: {
     adapter: adapter(),
